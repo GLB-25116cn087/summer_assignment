e25ce0b56a31fe0e851a65d5606bbf6752f981fb
GLB-25116cn087 <rukmanibst96@gmail.com>
2026-06-07 15:30:37 +0530

    QUESTION_24.C "FIND X^N WITHOUT POW()

---
+++ WEEK_1/DAY_6/QUESTION_24.C
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int x, n, i;
+    int result = 1;
+    printf("Enter base and power: ");
+    scanf("%d %d", &x, &n);
+    for(i = 1; i <= n; i++) {
+        result *= x;
+    }
+ printf("%d^%d = %d", x, n, result);
+return 0;
+}
