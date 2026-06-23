e204cf1429575b8be776f37794c49bb071861985
GLB-25116cn087 <rukmanibst96@gmail.com>
2026-06-23 13:53:19 +0530

    QUESTION_91.C " CHECTER ANAGRAM STRINGS  "

---
+++ WEEK_4/DAY_23/QUESTION_91.C
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    char str1[100], str2[100];
+    int count1[26] = {0}, count2[26] = {0};
+    int i, flag = 1;
+
+    printf("Enter first string: ");
+    gets(str1);
+
+    printf("Enter second string: ");
+    gets(str2);
+
+    for(i=0; str1[i]!='\0'; i++)
+    {
+        count1[str1[i]-'a']++;
+    }
+
+    for(i=0; str2[i]!='\0'; i++)
+    {
+        count2[str2[i]-'a']++;
+    }
+
+    for(i=0; i<26; i++)
+    {
+        if(count1[i] != count2[i])
+        {
+            flag = 0;
+            break;
+        }
+    }
+
+    if(flag == 1)
+        printf("Strings are anagram");
+    else
+        printf("Strings are not anagram");
+
+    return 0;
+}
